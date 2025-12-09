c3bbe5419f39af6f54960d52614d54b88e928a7c
mywebapp1221 <hirano.r1221@gmail.com>
2025-12-09 18:25:30 +0900

    a

--- firebase-config.js
+++ firebase-config.js
@@ -5,7 +5,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyDP1LgZclt3Atv2B1Z4aWNMruxfIHGnpWc",
   authDomain: "komorebi1221-c3dd8.firebaseapp.com",
   projectId: "komorebi1221-c3dd8",
-  storageBucket: "komorebi1221-c3dd8.firebasestorage.app",
+  storageBucket: "komorebi1221-c3dd8.appspot.com",
   messagingSenderId: "72244772206",
   appId: "1:72244772206:web:f3cc92e93f08292d17157f",
 };
